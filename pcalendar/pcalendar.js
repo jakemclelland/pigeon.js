5542f3d21c57094f1fe2f45e17eef94bc446ee2f
jake.mclelland <jake.mclelland@gmail.com>
2022-05-02 10:40:05 -0400

    Stubbed in initial files

---
+++ pcalendar/pcalendar.js
@@ -0,0 +1,32 @@
+const pcalendar = {
+    
+    options: {
+        
+    },
+
+    monthArray : ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+
+    daysArray : ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+
+    _renderWeekline: function(weekElement) {
+        weekElement.empty();
+    },
+
+    _renderCalendar: function(month, year) {
+
+    },
+
+    init : function(targetElementSelector) {
+        let calendarElement = document.querySelector(targetElementSelector);
+        console.log("calendarElement:", calendarElement);
+
+        const headerHtml = '<div class="pcal-header"><button class="pcal-shift-mon pcal-shift-past">back</button><h2></h2><button class="pcal-shift-mon pcal-shift-future">next</div>';
+        const bodyHtml = '<div class="pcal-weekdays"></div><div class"pcal-content"></div>';
+        const hiddenHtml = '<input type="hidden" id="pcal-start-date" value=""><input type="hidden" id="pcal-end-date" value="">';
+        calendarElement.innerHTML = headerHtml + bodyHtml + hiddenHtml;
+
+        let today = new Date();
+        this._renderCalendar(today.getFullYear(), today.getMonth());
+    },
+
+}
