9da0386a2eebbb34e76f166a27b3d0fdf7f2d274
jake.mclelland <jake.mclelland@gmail.com>
2022-07-25 18:56:08 -0400

    referenced general site style and license

--- pcalendar/pcalendar.js
+++ pcalendar/pcalendar.js
@@ -1,3 +1,10 @@
+/*
+* pcalendar v0.1 - a simple calendar control
+* Copyright (c) 2022 Jake McLelland
+* License: MIT
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
 const pcalendar = {
     
     options: {
